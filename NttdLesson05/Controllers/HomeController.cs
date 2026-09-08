1da63cf2f3fe81143834ac1cb4917e64a58d8d5a
Nttdat <thanhthanh15061999@gmail.com>
2026-09-08 10:36:41 +0700

    Nttdlesson05

---
+++ NttdLesson05/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using NttdLesson05.Models;
+
+namespace NttdLesson05.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        // Lập trình trên view với RazorCode
+        public IActionResult RazorCode()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
