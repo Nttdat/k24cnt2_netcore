28b9ada6f922696f2d65f40d120310c68848f165
Nttdat <thanhthanh15061999@gmail.com>
2026-09-09 19:11:44 +0700

    NttdLesson06Views

---
+++ NttdLesson06Views/Controllers/NttdHomeController.cs
@@ -0,0 +1,25 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using NttdLesson06Views.Models;
+
+namespace NttdLesson06Views.Controllers
+{
+    public class NttdHomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ NttdLesson06Views/ViewComponents/CategoryViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using NttdLesson06Views.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NttdLesson06Views.ViewComponents
+{
+    public class CategoryViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(int? n)
+        {
+            var categories = new List<Category>
+            {
+                new Category { CategoryID = 1, CategoryName = "Electronics", Status = true },
+                new Category { CategoryID = 2, CategoryName = "Books", Status = true },
+                new Category { CategoryID = 3, CategoryName = "Clothing", Status = false },
+                new Category { CategoryID = 4, CategoryName = "Home & Kitchen", Status = true }
+            };
+            n=n ?? 0;
+            var search = categories.Where(x=>x.CategoryID >n).ToList();
+            return View(search);
+        }
+    }
+}
